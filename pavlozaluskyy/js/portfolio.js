cfa786a40540d04f78f7d55f5b1b6b52a11b6470
PavloZaluskyy <pavlozaluskiy@gmail.com>
2019-03-25 21:42:25 +0200

    Update portfolio.js

--- pavlozaluskyy/js/portfolio.js
+++ pavlozaluskyy/js/portfolio.js
@@ -7,9 +7,14 @@ new Vue({
 	el: '#ell',
 	data: {
 		posts: [
-		{id: 1, url_img:'img/slides/1491233764815_501364.png', url_site: 'http://testqwe.zzz.com.ua/', title: 'No Limits', description: 'Website-Landing Ukrainian rock band "NO Limits"'},
-		{id: 2, url_img:'img/slides/webSpiders.PNG', url_site: 'https://pavlozaluskyy.github.io/webSpiders/', title: 'Web Spider', description: 'Web site with animation on CSS and JS!'}
-		
+		{id: 1, url_img:'img/slides/Michelangelo.jpg', url_site: 'https://pavlozaluskyy.github.io/Michelangelo/', title: 'Michelangelo', description: 'Just a site of an architectural company!'},
+		{id: 2, url_img:'img/slides/accnatua.jpg', url_site: 'http://acc.net.ua/', title: 'ACC.NET.UA', description: 'Online store of accessories for smartphones!'},
+		{id: 3, url_img:'img/slides/webSpiders.PNG', url_site: 'https://pavlozaluskyy.github.io/webSpiders/', title: 'Web Spider', description: 'Web site with animation on CSS and JS!'},
+		{id: 4, url_img:'img/slides/pavlozaluskyy.jpg', url_site: 'http://www.pavlozaluskyy.zzz.com.ua/', title: 'Pavlo Zaluskyy', description: 'My first site!'},
+		{id: 5, url_img:'img/slides/1491233764815_501364.png', url_site: 'http://testqwe.zzz.com.ua/', title: 'No Limits', description: 'Website-Landing Ukrainian rock band "NO Limits"'},
+		{id: 6, url_img:'img/slides/PortLand.jpg', url_site: 'https://pavlozaluskyy.github.io/Portland/', title: 'PortLand', description: 'Front-end of the page of the online store!'},
+		{id: 7, url_img:'img/slides/world.jpg', url_site: 'https://pavlozaluskyy.github.io/world/', title: 'MyWork', description: 'A simple web-app for learning English!'},
+
 		]
 	}
 
